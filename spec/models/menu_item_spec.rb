1962304a66e87901ac8a59b934a286737694c0d9
Bob Roberts <cardsfanbob@gmail.com>
2021-10-23 17:21:19 -0500

    Initial Level 1 done

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe MenuItem, type: :model do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:description) }
   end
+
+  describe 'create' do
+    it 'creates with required fields' do
+      menu = Menu.create(name: "Joes")
+      expect(MenuItem.create(menu: menu,
+                             title: 'Burger',
+                             description: "Delicous Cheeseburger",
+                             price: 9.99).save).to eq true
+    end
+  end
 end
 
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Menu, type: :model do
   context 'validations' do
     it { is_expected.to validate_presence_of(:name) }
   end
+
+  describe 'create' do
+    it 'creates with required fields' do
+      expect(Menu.create(name: "Joes").save).to eq true
+    end
+  end
 end
